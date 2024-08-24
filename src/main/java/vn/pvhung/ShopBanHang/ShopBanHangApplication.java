476f87778fc040ba5d34927d054f0d40e4018ab2
pvhung <hungdev27102003@gmail.com>
2024-08-24 21:00:06 +0700

    init

---
+++ src/main/java/vn/pvhung/ShopBanHang/ShopBanHangApplication.java
@@ -0,0 +1,13 @@
+package vn.pvhung.ShopBanHang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopBanHangApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopBanHangApplication.class, args);
+	}
+
+}
---
+++ src/test/java/vn/pvhung/ShopBanHang/ShopBanHangApplicationTests.java
@@ -0,0 +1,13 @@
+package vn.pvhung.ShopBanHang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopBanHangApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
